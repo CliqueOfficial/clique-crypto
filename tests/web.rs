91d0355b94f67436bf5fbec741c503a495c8bd40
Alan WANG <alan1995wang@outlook.com>
2024-01-29 14:23:44 +0800

    chore: add ci

--- tests/web.rs
+++ tests/web.rs
@@ -5,9 +5,23 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
+use clique_crypto::AES;
+
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
-    assert_eq!(1 + 1, 2);
+    use k256::{
+        elliptic_curve::{bigint::Uint, sec1::ToEncodedPoint},
+        NonZeroScalar, PublicKey, SecretKey,
+    };
+
+    let alice = [1u8; 32];
+    let sk = SecretKey::from_slice(&alice).unwrap();
+    let pk = sk.public_key();
+
+    AES::with_ecdh(
+        sk.to_bytes().as_slice(),
+        pk.to_encoded_point(false).as_bytes(),
+    );
 }
